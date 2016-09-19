fd321f07dc86a187007b638dfa7e127c8e2c829f
MaksimShumko <mszumko@gmail.com>
2016-09-19 23:49:33 +0200

    Rejected commit: Default

--- AndroidStudioProjects/CalculatorPKO/app/src/main/java/com/maksim/calculatorpko/Settings.java
+++ AndroidStudioProjects/CalculatorPKO/app/src/main/java/com/maksim/calculatorpko/Settings.java
@@ -5,23 +5,13 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.Set;
 
 public class Settings extends Activity implements View.OnClickListener {
     private TextView tv_operation1, tv_operation2, tv_interval1, tv_interval2,
@@ -42,6 +32,14 @@ public class Settings extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_settings);
+
+        Button b = (Button) findViewById(R.id.button_finish);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         Intent in = getIntent();
 
